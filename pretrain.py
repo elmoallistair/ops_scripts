5c6554ad113e8a327bd1ca49644176118b69b484
Elmo <work.elmoallistair@gmail.com>
2023-10-07 21:53:42 +0700

    Create pretrain.py

---
+++ pretrain.py
@@ -0,0 +1,39 @@
+def clean_review_text(raw_review):
+    raw_review = str(raw_review)
+    review_transform = raw_review.lower()
+    review_transform = review_transform.translate(str.maketrans('', '', string.punctuation))
+    review_transform = re.sub('\s+', ' ', review_transform).strip()
+    if re.search('[a-zA-Z]', review_transform):
+        return review_transform
+    else:
+        return None
+
+def transform_class(tag):
+    tag = re.sub(rf'[{string.punctuation}]', ' ', tag)
+    tag = re.sub(r'\s+', ' ', tag)
+    tag = tag.lower().strip().replace(' ', '_')
+    return tag
+
+def clean_sample_data(df_reviews):
+    old_len = len(df_reviews)
+    df_reviews['review_transform'] = df_reviews['review'].apply(clean_review_text)
+    df_reviews['tag_transform'] = df_reviews['tag'].apply(transform_class)
+    df_reviews.dropna(inplace=True)
+
+    df_reviews['word_len'] = df_reviews['review'].apply(lambda x: len(x.split()))
+    df_reviews['string_len'] = df_reviews['review'].apply(lambda x: len(x))
+
+    value_counts = df_training['tag'].value_counts()
+    df_reviews = df_reviews[df_reviews['tag'].isin(value_counts.index[value_counts >= 10])]
+
+    print(f'Removed {old_len - len(df_reviews)} rows of unmatched criteria')
+    return df_reviews
+
+def remove_duplicated_sample(df_reviews):
+    df_reviews['join'] = df_reviews['tag'] + ' ' + df_reviews['review']
+    df_reviews['occurrence'] = df_reviews.groupby('review')['join'].transform('count')
+    df_reviews = df_reviews.sort_values(by='occurrence', ascending=False)
+    df_reviews = df_reviews.drop_duplicates(subset='review', keep='first')
+    df_reviews.reset_index(drop=True, inplace=True)
+    
+    return df_reviews[['review_transform', 'tag_transform', 'occurrence']]
