0a99a3b44763505182f88bbccf699aa52e654471
Elmo <work.elmoallistair@gmail.com>
2023-10-26 08:33:59 +0700

    adding multi model predictions

--- predictor.py
+++ predictor.py
@@ -1,5 +1,7 @@
-import pandas as pd
+import os
+import joblib
 import numpy as np
+import pandas as pd
 
 def get_prediction_with_model(df_review, model, vectorizer, col_name):
     """
@@ -53,22 +55,53 @@ def get_prediction_with_record(df_review, df_record, col_name, min_occurrence=2)
     confidences = np.minimum(1.0, confidences)
     confidences[pd.isna(predictions)] = 0
 
-    # Format confidences with two decimal places
     confidences = [f"{conf:.2f}" for conf in confidences]
 
     return predictions, confidences
 
-def ensemble_prediction(df, method):
+def multi_model_prediction(df_reviews, review_col, task_id, model_path, model_ids):
     """
-    Combining multiple model prediction, based on methods.
+    Predict using multiple machine learning models on text data.
 
-    Args:
-        df: A Pandas DataFrame containing the data to be predicted.
-        method: The method to use for ensemble prediction. Can be either "majority_voting", "weighted_average",
-                or "highest_confidence".
+    Parameters:
+    - df_reviews (DataFrame): A DataFrame containing the text data to be used for predictions.
+    - review_col (str): The name of the column in df_reviews that contains the text data.
+    - task_id (str): A string specifying the task identifier.
+    - model_path (str): The directory path where the trained models and vectorizers are stored.
+    - model_ids (list of str): A list of model identifiers to indicate which models to use for predictions.
 
     Returns:
-        A Pandas DataFrame containing the ensemble predictions.
+    - df_preds (DataFrame): A DataFrame containing the original review text and predictions made by each of the specified models. 
+      Each model's predictions are stored in separate columns with names like 'pred_model_id'. 
+    """
+
+    df_preds = df_reviews[[review_col]].copy()
+    for model_id in model_ids:
+        print(f'[INFO] Predicting with {model_id}')
+        clf = joblib.load(os.path.join(model_path, f'{task_id}_model_latest_{model_id}.joblib'))
+        vec = joblib.load(os.path.join(model_path, f'{task_id}_feature_latest_{model_id}.joblib'))
+        pred, conf = get_prediction_with_model(df_preds, clf, vec, review_col)
+        df_preds[f'pred_{model_id}'] = list(zip(pred, conf))
+
+    return df_preds
+    
+def ensemble_prediction(df, method):
+    """
+    Generate ensemble predictions based on the specified method.
+
+    Parameters:
+    - df (DataFrame): A DataFrame containing individual model predictions in separate columns.
+    - method (str): The ensemble method to be used. Supported methods are 'majority_voting', 'weighted_average', and 'highest_confidence'.
+
+    Returns:
+    - df_ensemble (DataFrame): A DataFrame with two columns: 'final_prediction' and 'final_confidence', containing the ensemble prediction and confidence scores.
+
+    Additional Information:
+    - This function calculates ensemble predictions for each row in the DataFrame by considering the individual model predictions and their confidence scores.
+    - The 'method' parameter determines the ensemble strategy used.
+        - 'majority_voting' selects the most commonly predicted class.
+        - 'weighted_average' uses the class with the highest average confidence score.
+        - 'highest_confidence' selects the class with the highest confidence score.
     """
 
     pred_cols = [col for col in df.columns if col.startswith("pred_")]
@@ -106,3 +139,61 @@ def ensemble_prediction(df, method):
 
     return df_ensemble
 
+def get_all_ensemble_prediction(df_preds):
+    """
+    Perform ensemble predictions using different methods and add the results as new columns to the input DataFrame.
+
+    Parameters:
+    - df_preds (DataFrame): A DataFrame containing model predictions to be used for ensemble methods.
+
+    Additional Information:
+    - This function calls the 'ensemble_prediction' function with three different ensemble methods: majority voting,
+      weighted average, and highest confidence.
+    - The ensemble results for each method are added as new columns to the input DataFrame.
+
+    Returns:
+    - df_preds (DataFrame): The input DataFrame with added columns for ensemble predictions using different methods.
+
+    Example Usage:
+    get_ensemble_prediction(predictions_df)
+    """
+    ensemble_vot_result_list = list(ensemble_prediction(df_preds, method="majority_voting").itertuples(index=False))
+    ensemble_avg_result_list = list(ensemble_prediction(df_preds, method="weighted_average").itertuples(index=False))
+    ensemble_con_result_list = list(ensemble_prediction(df_preds, method="highest_confidence").itertuples(index=False))
+
+    df_preds["pred_ensemble_vot"] = ensemble_vot_result_list
+    df_preds["pred_ensemble_avg"] = ensemble_avg_result_list
+    df_preds["pred_ensemble_con"] = ensemble_con_result_list
+
+    return df_preds
+
+def adhoc_get_models_accuracies(df_preds, cols_pred_identifier, col_actual_tag):
+    """
+    Calculate accuracies for multiple model predictions.
+
+    Parameters:
+    - df_preds (DataFrame): A DataFrame containing the model predictions, where each model's predictions are stored in separate columns.
+    - cols_pred_identifier (str): A prefix that identifies the columns containing model predictions.
+    - col_actual_tag (str): The name of the column in df_preds that contains the actual target labels for the predictions.
+
+    Returns:
+    - accuracy_summary (DataFrame): A DataFrame summarizing the accuracy of each model's predictions. It lists the model column names (identified by cols_pred_identifier) and their respective accuracy values.
+    """
+     
+    def calculate_accuracy(pred_column, actual_column):
+        pred_class = [pred[0] for pred in pred_column]
+        total_samples = len(actual_column)
+        correct_predictions = (pred_class == actual_column).sum()
+        accuracy = correct_predictions / total_samples
+        return accuracy
+
+    accuracies = {}
+
+    for col in df_preds.columns:
+        if col.startswith(cols_pred_identifier):
+            accuracy = calculate_accuracy(df_preds[col], df_preds[col_actual_tag])
+            accuracies[col] = accuracy
+
+    accuracy_summary = pd.DataFrame.from_dict(accuracies, orient='index', columns=['accuracy'])
+    
+    return accuracy_summary
