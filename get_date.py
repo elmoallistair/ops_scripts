ad8d2265e222eb7c39069b1d6eda9f36cf3f5b17
Elmo <work.elmoallistair@gmail.com>
2023-11-25 23:54:13 +0700

    Update get_date.py

--- get_date.py
+++ get_date.py
@@ -1,17 +1,7 @@
 from datetime import datetime, timedelta
 
 def get_last_week_date_range(start_day="monday"):
-    """
-    Retrieve previous week's date range.
-
-    Args:
-    - start_day (str, optional): The starting day of the week (monday or sunday). Default is "monday".
-
-    Returns:
-    A tuple of two strings representing the date range. The first string is the start date, and the second string is the end date.
-    """
     current_date = datetime.now()
-    current_weeknum = current_date.isocalendar()[1]
 
     if start_day == 'monday':
         current_start_day = current_date - timedelta(days=current_date.weekday())
@@ -25,3 +15,34 @@ def get_last_week_date_range(start_day="monday"):
     date_end = previous_week_end.strftime('%Y-%m-%d')
     
     return date_start, date_end
+
+def get_n_last_date_range(n):
+    today_date = datetime.today()
+    date_format = "%Y-%m-%d"
+    
+    if n == 0:
+        date_start = date_end = today_date.strftime(date_format)
+    elif n == 1:
+        yesterday_date = today_date - datetime.timedelta(days=1)
+        date_start = yesterday_date.strftime(date_format)
+        date_end = today_date.strftime(date_format)
+    else: 
+        date_start = (today_date - timedelta(days=n)).strftime(date_format)
+        date_end = today_date.strftime(date_format)
+
+    return date_start, date_end
+
+def get_date_range(start_date, end_date):
+    date_list = []
+    current_date = datetime.strptime(start_date, '%Y-%m-%d')
+
+    while current_date <= datetime.strptime(end_date, '%Y-%m-%d'):
+        date_list.append(current_date.strftime('%Y-%m-%d'))
+        current_date += timedelta(days=1)
+
+    return date_list
+
+def get_last_week_monday():
+    today = datetime.now().date()
+    last_week_monday = today - timedelta(days=today.weekday() + 7)
+    return last_week_monday.strftime('%Y-%m-%d')
