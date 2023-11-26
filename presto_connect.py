91eb782310218fe0cae6a7939e57c9bf3519c7b5
Elmo <work.elmoallistair@gmail.com>
2023-11-26 18:31:11 +0700

    add retrieve_chat_data

--- presto_connect.py
+++ presto_connect.py
@@ -17,3 +17,11 @@ def execute_presto_query(query, username, password):
         print(f'An error occurred: {str(e)}')
         return None
 
+def retrieve_chat_data(base_query, df_keywords, date_start, date_end, username, password):
+    true_keywords = '|'.join(df_keywords['true_keywords'].replace('', np.nan).dropna().astype(str).str.strip())
+    false_keywords = '|'.join(df_keywords['false_keywords'].replace('', np.nan).dropna().astype(str).str.strip())
+
+    query = base_query.format(date_start=date_start, date_end=date_end, true_keywords=true_keywords, false_keywords=false_keywords)
+    df_chat_raw = execute_presto_query(query, username, password)
+
+    return df_chat_raw
