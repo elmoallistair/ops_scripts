69ebf4bf2f91b5cc838c712d62ae3b9eabcd3295
Elmo <work.elmoallistair@gmail.com>
2023-12-19 21:43:34 +0700

    add periodic rides and coc retriever

--- pipe.py
+++ pipe.py
@@ -13,3 +13,28 @@ def get_shared_treatment_tickets(df_tickets, disposition, cols_order, fw_exclude
     df_tickets['action_date'] = datetime.now().strftime('%Y-%m-%d')
 
     return df_tickets
+
+def get_periodic_rides_data(identifier, sheet_source, period_name, period_range):
+    df_rides = gconnect.read_data_from_sheet(client, sheet_source, identifier, verbose=True)
+    df_rides['period'] = df_rides[period_name].astype(int)
+    df_rides = df_rides[df_rides['period'].isin(period_range)]
+    df_rides.drop(columns=['week', 'month'], inplace=True)
+
+    return df_rides
+
+def get_periodic_coc_data(df_coc, period_name, period_range, cols_order):
+    df_coc['period'] = df_coc[period_name].astype(int)
+    df_coc['date_local'] = pd.to_datetime(df_coc['date_local'])
+
+    df_coc = df_coc[df_coc['remarks'] == 'Valid']
+    df_coc = df_coc[df_coc['period'].isin(period_range)]
+
+    df_coc.sort_values('date_local', inplace=True)
+    df_coc.drop_duplicates(subset=['booking_code', 'source_data'], inplace=True)
+    df_coc.drop_duplicates(subset=['driver_id', 'date_local', 'disposition'], inplace=True)
+
+    print(df_coc['period'].value_counts().sort_index())
+    df_coc = df_coc[cols_order]
+    df_coc = df_coc[df_coc['vertical'].isin(['GrabFood', 'GrabMart', 'GrabExpress'])]
+    
+    return df_coc
