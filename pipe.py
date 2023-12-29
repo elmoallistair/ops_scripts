61c58a6963fab363b6b3855090a1f55645f13131
Elmo <work.elmoallistair@gmail.com>
2023-12-29 23:43:05 +0700

    add safety and maps complain

--- pipe.py
+++ pipe.py
@@ -14,6 +14,35 @@ def get_shared_treatment_tickets(df_tickets, disposition, cols_order, fw_exclude
 
     return df_tickets
 
+def get_safety_and_maps_complains(df_tickets, cols_order, reporter='pax', verbose=True):
+    df_maps = df_tickets[df_tickets.sub_disposition == 'Grab Maps']
+    df_safety = df_tickets[df_tickets.sub_disposition == 'Safety']
+
+    if reporter == 'pax':
+        df_safety['reporter'] = 'Passenger'
+        df_safety['reporter_id'] = df_safety['passenger_id']
+        df_maps['reporter'] = 'Passenger'
+        df_maps['reporter_id'] = df_maps['passenger_id']
+    elif reporter == 'dax':
+        df_safety['reporter'] = 'Driver'
+        df_safety['reporter_id'] = df_safety['driver_id']
+        df_maps['reporter'] = 'Passenger'
+        df_maps['reporter_id'] = df_maps['passenger_id']
+
+    df_maps['category'] = 'Grab Maps'
+    df_maps['date_today'] = datetime.date.today().strftime('%Y-%m-%d')
+    df_maps['action_date'] = datetime.date.today().strftime('%Y-%m-%d')
+    df_safety['action_date'] = datetime.date.today().strftime('%Y-%m-%d')
+    
+    df_maps = processing.order_column_by_template(df_maps, cols_order['routing'])
+    df_safety = processing.order_column_by_template(df_safety, cols_order['safety'])
+        
+    if verbose:
+        print(f'[INFO] Retrieved {len(df_maps)} maps tickets')
+        print(f'[INFO] Retrieved {len(df_safety)} safety tickets')
+
+    return df_safety, df_maps
+
 def get_periodic_rides_data(identifier, sheet_source, period_name, period_range):
     df_rides = gconnect.read_data_from_sheet(client, sheet_source, identifier, verbose=True)
     df_rides['period'] = df_rides[period_name].astype(int)
